3f2fe6e8f32dad31804dd863f70640ea017e7d32
Gurdeep Singh <g.s.basantpuria@gmail.com>
2025-01-05 23:20:45 -0500

    edit button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Button() {
   return (
-    <div class="bg-yellow-600 px-4 py-1 rounded-sm cursor-pointer cursor hover:bg-yellow-500">
+    <button className="bg-yellow-600 px-4 py-1 rounded-sm cursor-pointer cursor hover:bg-yellow-500">
       Get a Joke
-    </div>
+    </button>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Button from "./Button";
+import LogoutBtn from "./Header/LogoutBtn";
 import Container from "./container/Container";
 import JokeCard from "./JokeCard";
 import Logo from "./Logo";
 
-export { Header, Footer, Button, JokeCard, Logo, Container };
+export { Header, Footer, Button, LogoutBtn, JokeCard, Logo, Container };
