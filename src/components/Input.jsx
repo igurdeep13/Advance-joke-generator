9dde147cf665fe42a15d709811eb102c41c4195b
Gurdeep Singh <g.s.basantpuria@gmail.com>
2025-01-06 21:57:45 -0500

    config input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,17 @@ const Input = React.forwardRef(function Input(
   { label, type = "text", className = "", ...props },
   ref
 ) {
-  return <div className="w-full"></div>;
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="inline-block mb-1 pl-1 " htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input type={type} className="" ref={ref} {...props} id={id} />
+    </div>
+  );
 });
 
 export default Input;
