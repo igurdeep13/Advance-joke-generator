905049568292fd7aa15bdcee4a6a19d6480a5d05
Gurdeep Singh <g.s.basantpuria@gmail.com>
2025-01-09 19:19:43 -0500

    create AuthLayout

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-function Protected({ Children, authentication = true }) {
-  const navigate = useState();
+function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
-  return <div>AuthLayout</div>;
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
 }
 
 export default AuthLayout;
